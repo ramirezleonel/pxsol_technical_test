016498056b7f8efb5d342395c45839bdd2600ebf
ramirezleonel <ramirezleonel0@gmail.com>
2020-12-20 00:18:19 -0300

    se modificó migracion por defecto de users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name',255);
+            $table->string('last_name',255);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
